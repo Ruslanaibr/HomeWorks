735504c1b3531ea0d2aecfdffdee549d74a2b84e
Ruslanaibr <101861977+Ruslanaibr@users.noreply.github.com>
2023-04-24 17:59:56 +0300

    lesson 4

---
+++ HomeWorks/PlayerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlayerViewController.swift
+//  HomeWorks
+//
+//  Created by Руслана ибрагимова on 24.04.2023.
+//
+
+import UIKit
+import AVFoundation
+
+class PlayerViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayouts()
+        view.backgroundColor = .white
+    }
+    
+    private func setupLayouts() {
+        
+    }
+    
+}
--- HomeWorks/ViewController.swift
+++ HomeWorks/ViewController.swift
@@ -38,23 +38,14 @@ final class ViewController: UIViewController {
         configureCellStack2()
     }
     
+    //Smeshariki button pressed
     
-    //View
-    private func configureView () {
-        view.backgroundColor                                     = .white
-        
-        navigationController?.navigationBar.prefersLargeTitles   = true
-        
-        let appearance                                           = UINavigationBarAppearance()
-        appearance.backgroundColor                               = Const.Color.green
-        appearance.titleTextAttributes                           = [.foregroundColor: UIColor.white]
-        appearance.largeTitleTextAttributes                      = [.foregroundColor: UIColor.white]
-
-        navigationController?.navigationBar.tintColor            = .white
-        navigationController?.navigationBar.standardAppearance   = appearance
-        navigationController?.navigationBar.compactAppearance    = appearance
-        navigationController?.navigationBar.scrollEdgeAppearance = appearance
-        
+    @objc private func button1Pressed () {
+        navigationController?.pushViewController(PlayerViewController(), animated: true)
+    }
+    
+    @objc private func button2Pressed () {
+        navigationController?.pushViewController(PlayerViewController(), animated: true)
     }
     
     // 1 Song
@@ -80,16 +71,41 @@ final class ViewController: UIViewController {
         }
     }
     
+    //View
+    private func configureView () {
+        view.backgroundColor                                     = .white
+        
+        navigationController?.navigationBar.prefersLargeTitles   = true
+        title = "PlayList"
+        
+        let appearance                                           = UINavigationBarAppearance()
+        appearance.backgroundColor                               = Const.Color.green
+        appearance.titleTextAttributes                           = [.foregroundColor: UIColor.white]
+        appearance.largeTitleTextAttributes                      = [.foregroundColor: UIColor.white]
+
+        navigationController?.navigationBar.tintColor            = .white
+        navigationController?.navigationBar.standardAppearance   = appearance
+        navigationController?.navigationBar.compactAppearance    = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+    }
+    
     
     //Main Stack
     private func configureMainStack () {
         view.addSubview(mainStack)
         
-        mainStack.frame = view.bounds
-        mainStack.heightAnchor.constraint(equalToConstant: view.frame.height/2).isActive = true
         mainStack.axis = .vertical
-        mainStack.spacing = 20
+        mainStack.spacing = 10
         mainStack.distribution = .fillEqually
+        mainStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainStack.heightAnchor.constraint(equalToConstant: view.frame.height/4),
+            mainStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            mainStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
         
         mainStack.addArrangedSubview(cellStack1)
         mainStack.addArrangedSubview(cellStack2)
@@ -101,44 +117,53 @@ final class ViewController: UIViewController {
     private func configureCellStack1 () {
         cellStack1.axis = .horizontal
         cellStack1.spacing = 20
-        cellStack1.distribution = .fill
-        cellStack1.alignment = .fill
+        cellStack1.distribution = .fillProportionally
+
         
         cellStack1.addArrangedSubview(songImage1)
         cellStack1.addArrangedSubview(songButton1)
         cellStack1.addArrangedSubview(durLabel1)
         
-        
-        durLabel1.text = String(song1.duration)
+        let min = Int(song1.duration/60)
+        let sec = Int(song1.duration.truncatingRemainder(dividingBy: 60))
+        durLabel1.text = "\(min)m \(sec)s"
+
         songButton1.setTitle("Smeshariki - Ot Vinta!.mp3", for: .normal)
         songButton1.setTitleColor(.black, for: .normal)
+        songButton1.titleLabel?.numberOfLines = 0
+        songButton1.addTarget(self, action: #selector(button1Pressed), for: .touchUpInside)
+        
+        //image1
         songImage1.image = UIImage(named: "smeshariki")
         songImage1.contentMode = .scaleAspectFit
         
         NSLayoutConstraint.activate([
-            songImage1.widthAnchor.constraint(equalToConstant: 70),
-            songImage2.heightAnchor.constraint(equalToConstant: 70)
+            songImage1.widthAnchor.constraint(equalToConstant: view.bounds.size.width/5)
         ])
     }
     //Cell Stack2
     private func configureCellStack2 () {
         cellStack2.axis = .horizontal
         cellStack2.spacing = 20
-        cellStack2.distribution = .fill
-        cellStack2.alignment = .fill
+        cellStack2.distribution = .fillProportionally
         
         cellStack2.addArrangedSubview(songImage2)
         cellStack2.addArrangedSubview(songButton2)
         cellStack2.addArrangedSubview(durLabel2)
         
-        durLabel2.text = String(song2.duration)
+        let min = Int(song2.duration/60)
+        let sec = Int(song2.duration.truncatingRemainder(dividingBy: 60))
+        durLabel2.text = "\(min)m \(sec)s"
         songButton2.setTitle("Mo Rodgers - Black Coffee.mp3", for: .normal)
         songButton2.setTitleColor(.black, for: .normal)
+        songButton2.titleLabel?.numberOfLines = 0
+        songButton2.addTarget(self, action: #selector(button2Pressed), for: .touchUpInside)
+        
+        //image2
         songImage2.image = UIImage(named: "blackCoffee")
         songImage2.contentMode = .scaleAspectFit
         NSLayoutConstraint.activate([
-            songImage1.widthAnchor.constraint(equalToConstant: 70),
-            songImage2.heightAnchor.constraint(equalToConstant: 70)
+            songImage2.widthAnchor.constraint(equalToConstant: view.bounds.size.width/5)
         ])
     }
     
